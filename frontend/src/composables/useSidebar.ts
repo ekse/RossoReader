f75668e26f407b443a4b9eeac7b33c978bc0d687
Philippe Miron <philippemiron@gmail.com>
2026-06-23 09:23:26 -0400

    feat: implement responsive collapsible sidebar and view headers with tests

---
+++ frontend/src/composables/useSidebar.ts
@@ -0,0 +1,22 @@
+import { ref } from 'vue'
+
+const isOpen = ref(true)
+
+export function useSidebar() {
+  function toggle() {
+    isOpen.value = !isOpen.value
+  }
+  function close() {
+    isOpen.value = false
+  }
+  function open() {
+    isOpen.value = true
+  }
+
+  return {
+    isOpen,
+    toggle,
+    close,
+    open,
+  }
+}
